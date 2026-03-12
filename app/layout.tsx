dd5a4505b4c2ed1b7bd54d1324f7761ed5b1da47
Alex Schmaltz <schmalaa@gmail.com>
2026-03-12 19:49:24 -0400

    fix: install and inject @vercel/analytics provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <div className="fixed inset-0 bg-grid-white opacity-20 pointer-events-none" />
         {children}
+        <Analytics />
       </body>
     </html>
   );
